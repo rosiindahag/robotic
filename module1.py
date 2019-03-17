d923fc91bb2a0855b2aee6e6b11f7b64f30f870f
shinaruki <44294018+shinaruki@users.noreply.github.com>
2019-03-17 12:14:36 +0700

    Update module1.py

--- module1.py
+++ module1.py
@@ -23,60 +23,26 @@ def turn(speed, seconds):
     time.sleep(seconds)
     R.motors[0].m0.power = 0
     R.motors[0].m1.power = 0
-
+tokens = filter(token_filter, R.see())
+m = tokens[0]
 i = 1
 state = SEARCHING
 while True:
-    #turn(25,1.5)
+    tokens = filter(token_filter, R.see())
     if state == SEARCHING:
-        print "Searching..."
-        tokens = filter(token_filter, R.see())
-        if len(tokens) > 0:
-            m = tokens[0]
-            print "Token sighted. {0} is {1}m away, bearing {2} degrees." \
-                  .format(m.info.offset, m.dist, m.rot_y)
+        if m.info.offset == i:
+            drive(100,1.4)
+            if R.grab():
+                turn(25, 2.4)
+                print("staaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaakkkkkkkkkk")
+                drive(100, 1)
+                if R.release():
+                    turn(-25,1.2)
+                    print("ed")
+            i = i+1
             state = DRIVING
-
         else:
-            print "Can't see anything."
-            state = DRIVING
-            #turn(25, 1.2)
-            #time.sleep(0.2)
-
-    elif state == DRIVING:
-        print "Aligning..."
-        tokens = filter(token_filter, R.see())
-        if len(tokens) == 0:
             state = SEARCHING
-
-        else:
-            m = tokens[0]
-            if m.info.offset == i:
-                i = i+1
-                if m.dist < 0.4:
-                    print "Found it!"
-                    if R.grab():
-                        print "Gotcha!"
-                        turn(100, -2.4)
-                        drive(100, 1)
-                        if R.release():
-                            turn(90,0.5)
-                            state = SEARCHING
-                    else:
-                        print "Aww, I'm not close enough."
-                #exit()
-
-                elif -15 <= m.rot_y <= 15:
-                    print "Ah, that'll do."
-                    drive(80, 1.5)
-
-
-                elif m.rot_y < -15:
-                    print "Left a bit..."
-                    turn(-12.5, 0.5)
-
-                elif m.rot_y > 15:
-                    print "Right a bit..."
-                    turn (12.5, 0.5)
-            else:
-                state = SEARCHING
+    elif state == DRIVING:
+        turn(25,1.2)
+        state = SEARCHING
